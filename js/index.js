b868513913b20a768d4673f99b94b9d2fd0756f1
piskun <piskun@corp.sputnik.ru>
2015-06-05 12:04:44 +0300

    ajax

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
 				$('a#play-last').addClass("error");
 			} // если
 			else { // вход в систему был успешным
-				$('a#play-now').text("data.error: " + data.status);
+				$('a#play-now').text("data.error: " + data.status.icestats.source.title);
 				//$('div#play-last').addClass("error");
 				
 			};
